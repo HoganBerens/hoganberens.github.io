61d39752abeab5de1726cd3ad17c684a4206857a
Hogan Berens <berenshogan@gmail.com>
2023-04-17 10:42:04 -0500

    changed my-golf-game dashboard design

--- src/components/my-golf-game/dashboard.jsx
+++ src/components/my-golf-game/dashboard.jsx
@@ -1,17 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./dashboard.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './dashboard.css';
 
 const Dashboard = () => {
-  const user = JSON.parse(sessionStorage.getItem("user"));
+  const user = JSON.parse(sessionStorage.getItem('user'));
 
   return (
     <div className="my-golf-game-dashboard-container">
-      {console.log(user)}
       <Link to="/">Back</Link>
-      <div className="mgg-dashboard-postedScores-container">Posted Scores</div>
-      <Link to="/newScoreDetails">Enter New Score</Link>
-      <Link to="/newCourse">Enter New Course</Link>
+      <Link to="/newScoreDetails">
+        <div className="my-golf-game-dashboard-card">Enter New Score</div>
+      </Link>
+      <Link>
+        <div className="my-golf-game-dashboard-card">Your Scores</div>
+      </Link>
     </div>
   );
 };
---
+++ src/components/my-golf-game/yourScores.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './yourScores.css';
+
+const YourScores = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default YourScores;
