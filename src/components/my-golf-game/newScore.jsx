7475146c77f6cbca5d821dc3f1dfd72d504a6635
Hogan Berens <berenshogan@gmail.com>
2023-05-09 14:22:22 -0500

    finished styling newScore component

--- src/components/my-golf-game/newScore.jsx
+++ src/components/my-golf-game/newScore.jsx
@@ -1,47 +1,46 @@
-import React, { useRef, useState, useEffect } from 'react';
-import './newScore.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { user, clubs, course } from './utils';
+import React, { useRef, useState, useEffect } from "react";
+import "./newScore.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { user, clubs, course } from "./utils";
 import {
   faCheckCircle,
   faCircleArrowDown,
   faCircleArrowUp,
   faCircleArrowLeft,
   faCircleArrowRight,
-} from '@fortawesome/free-solid-svg-icons';
+} from "@fortawesome/free-solid-svg-icons";
 
 const NewScore = () => {
   const [missedFairway, setMissedFairway] = useState(false);
   const [missedGreen, setMissedGreen] = useState(false);
   const [roundScore, setRoundScore] = useState([]);
-  const [pageReloaded, setPageReloaded] = useState(false);
 
   let missedFairwayLieOptions = [
-    'Rough',
-    'Bunker',
-    'In trees',
-    'Hazard',
-    'Other',
+    "Rough",
+    "Bunker",
+    "In trees",
+    "Hazard",
+    "Other",
   ];
   let missedGreenLieOptions = [
-    'Fairway cut',
-    'Rough',
-    'Bunker',
-    'In trees',
-    'Hazard',
-    'Other',
+    "Fairway cut",
+    "Rough",
+    "Bunker",
+    "In trees",
+    "Hazard",
+    "Other",
   ];
 
-  let clubHitOffTeeRef = useRef('');
-  let fairwayHitRef = useRef('');
-  let missedFairwayLieRef = useRef('');
-  let apprDistanceRef = useRef('');
-  let apprClubRef = useRef('');
-  let greenHitRef = useRef('');
-  let missedGreenLieRef = useRef('');
-  let secondPuttDistanceRef = useRef('');
-  let numberOfPuttsRef = useRef('');
-  let holeScoreRef = useRef('');
+  let clubHitOffTeeRef = useRef("");
+  let fairwayHitRef = useRef("");
+  let missedFairwayLieRef = useRef("");
+  let apprDistanceRef = useRef("");
+  let apprClubRef = useRef("");
+  let greenHitRef = useRef("");
+  let missedGreenLieRef = useRef("");
+  let secondPuttDistanceRef = useRef("");
+  let numberOfPuttsRef = useRef("");
+  let holeScoreRef = useRef("");
 
   let newScoreSubmitHandler = (event) => {
     event.preventDefault();
@@ -60,27 +59,27 @@ const NewScore = () => {
 
     setRoundScore((roundScore) => [...roundScore, holeResults]);
 
-    clubHitOffTeeRef.current.value = '';
-    fairwayHitRef.current = '';
-    missedFairwayLieRef.current = '';
-    apprDistanceRef.current.value = '';
-    apprClubRef.current = '';
-    greenHitRef.current = '';
-    missedGreenLieRef.current = '';
-    secondPuttDistanceRef.current = '';
-    numberOfPuttsRef.current = '';
-    holeScoreRef.current.value = '';
+    clubHitOffTeeRef.current.value = "";
+    fairwayHitRef.current = "";
+    missedFairwayLieRef.current = "";
+    apprDistanceRef.current.value = "";
+    apprClubRef.current = "";
+    greenHitRef.current = "";
+    missedGreenLieRef.current = "";
+    secondPuttDistanceRef.current = "";
+    numberOfPuttsRef.current = "";
+    holeScoreRef.current.value = "";
   };
 
   let fairwayHitHandler = (event) => {
     fairwayHitRef.current = event.target.id;
-    fairwayHitRef.current !== 'fairway'
+    fairwayHitRef.current !== "fairway"
       ? setMissedFairway(true)
       : setMissedFairway(false);
   };
   let greenHitHandler = (event) => {
     greenHitRef.current = event.target.id;
-    greenHitRef.current !== 'green'
+    greenHitRef.current !== "green"
       ? setMissedGreen(true)
       : setMissedGreen(false);
   };
@@ -93,7 +92,7 @@ const NewScore = () => {
     missedGreenLieRef.current = event.target.value;
   };
 
-  let holeNumber = '';
+  let holeNumber = "";
 
   for (let i = 0; i <= roundScore.length; i++) {
     holeNumber = i + 1;
@@ -101,87 +100,90 @@ const NewScore = () => {
 
   return (
     <div className=" newScore-container ">
-      <h1 id="startOfElement" className="newScore-title">
-        Hole: {holeNumber}
-      </h1>
-      <form className="newScore-form" onSubmit={newScoreSubmitHandler}>
-        <select
-          className="newScore-select"
-          ref={clubHitOffTeeRef}
-          name="selectedClubOffTee"
-        >
-          <option hidden={true} value="">
-            Select the club you hit off the tee
-          </option>
-          {clubs.map((club, index) => (
-            <option key={index}>{club}</option>
-          ))}
-        </select>
-        <div className="newScore-fairway-text-wrapper">
-          <div className="newScore-fairway-title">
-            Where did your tee shot go?
-          </div>
-          <div className="newScore-fairway-container">
-            <div className="newScore-fairway-hit">
-              <div>FIR</div>
-              <FontAwesomeIcon
-                icon={faCheckCircle}
-                onClick={fairwayHitHandler}
-                className="fairway-photo "
-                id="Faiway"
-              />
-            </div>
-            <div className="newScore-fairway-missed">
-              <div>Missed Fairway</div>
-              <FontAwesomeIcon
-                onClick={fairwayHitHandler}
-                id="left"
-                icon={faCircleArrowLeft}
-                className="fairway-green-input"
-              />
-              <FontAwesomeIcon
-                onClick={fairwayHitHandler}
-                id="Right"
-                icon={faCircleArrowRight}
-                className="fairway-green-input"
-              />
-              <FontAwesomeIcon
-                onClick={fairwayHitHandler}
-                id="Short"
-                icon={faCircleArrowDown}
-                className="fairway-green-input"
-              />
-              <FontAwesomeIcon
-                onClick={fairwayHitHandler}
-                icon={faCircleArrowUp}
-                id="Long"
-                className="fairway-green-input"
-              />
-            </div>
-          </div>
+      <div className="newScore-form-container container">
+        <div id="startOfElement" className="newScore-title row ">
+          Hole: {holeNumber}
         </div>
-        {missedFairway ? (
-          <select onChange={missedFairwayLieSelectHandler}>
-            <option hidden={true}>
-              What kind of lie did you have off the tee?
+        <form className="newScore-form row" onSubmit={newScoreSubmitHandler}>
+          <select
+            className="newScore-select col-md-4"
+            ref={clubHitOffTeeRef}
+            name="selectedClubOffTee"
+          >
+            <option hidden={true} value="">
+              Select the club you hit off the tee
             </option>
-            {missedFairwayLieOptions.map((lieOption, index) => (
-              <option ref={missedFairwayLieRef} key={index}>
-                {lieOption}
-              </option>
+            {clubs.map((club, index) => (
+              <option key={index}>{club}</option>
             ))}
           </select>
-        ) : (
-          <div></div>
-        )}
-        <input
-          id="approach-distance"
-          placeholder="Approach Distance"
-          type="text"
-          ref={apprDistanceRef}
-        />
-        <div className="newScore-appr-wrapper">
-          <select>
+          <div className="newScore-fairway-text-wrapper col-md-4">
+            <div className="newScore-fairway-container">
+              <div className="newScore-fairway-hit">
+                <div>FIR</div>
+                <FontAwesomeIcon
+                  icon={faCheckCircle}
+                  onClick={fairwayHitHandler}
+                  className="fairway-photo "
+                  id="Faiway"
+                />
+              </div>
+              <div className="newScore-fairway-missed">
+                <div>Missed Fairway</div>
+                <div className="newScore-arrows-wrapper">
+                  <FontAwesomeIcon
+                    onClick={fairwayHitHandler}
+                    id="left"
+                    icon={faCircleArrowLeft}
+                    className="fairway-green-input"
+                  />
+                  <FontAwesomeIcon
+                    onClick={fairwayHitHandler}
+                    id="Right"
+                    icon={faCircleArrowRight}
+                    className="fairway-green-input"
+                  />
+                  <FontAwesomeIcon
+                    onClick={fairwayHitHandler}
+                    id="Short"
+                    icon={faCircleArrowDown}
+                    className="fairway-green-input"
+                  />
+                  <FontAwesomeIcon
+                    onClick={fairwayHitHandler}
+                    icon={faCircleArrowUp}
+                    id="Long"
+                    className="fairway-green-input"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          {missedFairway ? (
+            <select
+              className="newScore-missedFaiway-select col-md-4"
+              onChange={missedFairwayLieSelectHandler}
+            >
+              <option hidden={true}>
+                What kind of lie did you have off the tee?
+              </option>
+              {missedFairwayLieOptions.map((lieOption, index) => (
+                <option ref={missedFairwayLieRef} key={index}>
+                  {lieOption}
+                </option>
+              ))}
+            </select>
+          ) : (
+            <div></div>
+          )}
+          <input
+            className="newScore-form-input col-md-4"
+            id="approach-distance"
+            placeholder="Approach Distance"
+            type="text"
+            ref={apprDistanceRef}
+          />
+          <select className=" newScore-appr-select col-md-4">
             <option hidden={true}>What club are you hitting?</option>
             {clubs.map((club, index) => (
               <option ref={apprClubRef} key={index}>
@@ -189,76 +191,81 @@ const NewScore = () => {
               </option>
             ))}
           </select>
-        </div>
-        <div>Where did your approach shot go?</div>
-        <div className="newScore-green-container">
-          <div className="newScore-green-hit">
-            <div>GIR</div>
-            <FontAwesomeIcon
-              icon={faCheckCircle}
-              onClick={greenHitHandler}
-              id="green"
-              className=" green-photo "
-            />
+          <div className="newScore-fairway-text-wrapper col-md-4">
+            <div className="newScore-fairway-container">
+              <div className="newScore-fairway-hit">
+                <div>GIR</div>
+                <FontAwesomeIcon
+                  icon={faCheckCircle}
+                  onClick={greenHitHandler}
+                  id="green"
+                  className=" green-photo "
+                />
+              </div>
+              <div className="newScore-fairway-missed">
+                <div>Missed Green</div>
+                <div className="newScore-arrows-wrapper">
+                  <FontAwesomeIcon
+                    id="left"
+                    onClick={greenHitHandler}
+                    icon={faCircleArrowLeft}
+                    className="fairway-green-input"
+                  />
+                  <FontAwesomeIcon
+                    id="right"
+                    onClick={greenHitHandler}
+                    icon={faCircleArrowRight}
+                    className="fairway-green-input"
+                  />
+                  <FontAwesomeIcon
+                    id="short"
+                    onClick={greenHitHandler}
+                    icon={faCircleArrowDown}
+                    className="fairway-green-input"
+                  />
+                  <FontAwesomeIcon
+                    id="long"
+                    onClick={greenHitHandler}
+                    icon={faCircleArrowUp}
+                    className=" fairway-green-input"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="newScore-green-missed">
-            <div>Missed Green</div>
-            <FontAwesomeIcon
-              id="left"
-              onClick={greenHitHandler}
-              icon={faCircleArrowLeft}
-              className="fairway-green-input"
-            />
-            <FontAwesomeIcon
-              id="right"
-              onClick={greenHitHandler}
-              icon={faCircleArrowRight}
-              className="fairway-green-input"
-            />
-            <FontAwesomeIcon
-              id="short"
-              onClick={greenHitHandler}
-              icon={faCircleArrowDown}
-              className="fairway-green-input"
-            />
-            <FontAwesomeIcon
-              id="long"
-              onClick={greenHitHandler}
-              icon={faCircleArrowUp}
-              className=" fairway-green-input"
-            />
-          </div>
-        </div>
-        {missedGreen ? (
-          <select onChange={missedGreenSelectHandler}>
-            <option hidden={true}>
-              What kind of lie did you have around the green?
-            </option>
-            {missedGreenLieOptions.map((greenLieOption, index) => (
-              <option ref={missedGreenLieRef} key={index}>
-                {greenLieOption}
+          {missedGreen ? (
+            <select onChange={missedGreenSelectHandler}>
+              <option hidden={true}>
+                What kind of lie did you have around the green?
               </option>
-            ))}
-          </select>
-        ) : (
-          <div></div>
-        )}
-        <input
-          className="input-putt-distance"
-          id="second-putt-distance"
-          placeholder="First Putt Distance"
-          type="text"
-          ref={secondPuttDistanceRef}
-        />
-        <input
-          className="input-hole-score"
-          id="hole-score"
-          placeholder="Hole Score"
-          type="text"
-          ref={holeScoreRef}
-        />
-        <button type="submit">Continue to Next Hole</button>
-      </form>
+              {missedGreenLieOptions.map((greenLieOption, index) => (
+                <option ref={missedGreenLieRef} key={index}>
+                  {greenLieOption}
+                </option>
+              ))}
+            </select>
+          ) : (
+            <div></div>
+          )}
+          <input
+            className="newScore-form-input col-md-4"
+            id="second-putt-distance"
+            placeholder="First Putt Distance"
+            type="text"
+            ref={secondPuttDistanceRef}
+          />
+          <input
+            className="newScore-form-input col-md-4"
+            id="hole-score"
+            placeholder="Hole Score"
+            type="text"
+            ref={holeScoreRef}
+          />
+          <button type="submit" className="col-md-4">
+            Continue to Next Hole
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
