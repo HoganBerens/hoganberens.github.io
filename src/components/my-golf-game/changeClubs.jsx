debf95d7d24b474c37cc54248ba17db01f78765a
Hogan Berens <berenshogan@gmail.com>
2023-05-17 13:36:44 -0500

    changed data in utils

--- src/components/my-golf-game/changeClubs.jsx
+++ src/components/my-golf-game/changeClubs.jsx
@@ -2,20 +2,35 @@ import React from "react";
 import "./changeClubs.css";
 import Header from "./header";
 import { clubs } from "./utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleMinus } from "@fortawesome/free-solid-svg-icons";
 
 const ChangeClubs = () => {
+  let clubSelectHandler = (event) => {
+    console.log(event.target);
+  };
+  let updateClubsHandler = () => {};
+
   return (
     <div className="changeClubs-container">
       <Header />
       <div className="changeClubs-content-wrapper container">
-        <div className="changeClubs-map-container row">
-          <div className="changeClubs-clubs-wrapper col-md-6">
-            {clubs.map((club, clubIndex) => (
-              <div className="changeClubs-clubs" key={clubIndex}>
+        <div className="changeClubs-clubs-wrapper col-md-6 ">
+          {clubs.map((club, clubIndex) => (
+            <div
+              className="changeClubs-club-container col-md-4"
+              key={clubIndex}
+            >
+              <div
+                className="changeClubs-club-name"
+                onClick={clubSelectHandler}
+              >
                 {club}
               </div>
-            ))}
-          </div>
+              <FontAwesomeIcon icon={faCircleMinus} />
+            </div>
+          ))}
+          <button onClick={updateClubsHandler}>Update Clubs</button>
         </div>
       </div>
     </div>
--- src/components/my-golf-game/yourScores.jsx
+++ src/components/my-golf-game/yourScores.jsx
@@ -1,95 +1,89 @@
-import React from 'react';
-import './yourScores.css';
-import { scores, course } from './utils';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import "./yourScores.css";
+import { scores, course } from "./utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faUser,
   faCheckCircle,
   faTimesCircle,
-} from '@fortawesome/free-regular-svg-icons';
-import { faMinusCircle } from '@fortawesome/free-solid-svg-icons';
-
+} from "@fortawesome/free-regular-svg-icons";
+import { faMinusCircle } from "@fortawesome/free-solid-svg-icons";
+import Header from "./header";
 const YourScores = () => {
   let allHoles = course.Holes;
-  let score = scores[0];
 
   return (
-    <div className="yourScores-wrapper container">
-      <div className="yourScores-header">
-        <Link to="/my-golf-game">Back</Link>
-        <div className="yourScores-header-title">Your Scores</div>
-        <Link to="/profile">
-          <FontAwesomeIcon className="fa-xl" icon={faUser} />
-        </Link>
-      </div>
-      <div className="yourScores-round-results-wrapper ">
-        {scores.map((score) => (
-          <div className="d-flex">
-            <div className="yourScores-round-values-wrapper">
-              <div className="yourScores-course-values-wrapper">
-                {[
-                  'Hole:',
-                  'Yards:',
-                  'Par:',
-                  'Score:',
-                  'FIR:',
-                  'Appr:',
-                  'GIR:',
-                  'Putts:',
-                ].map((value) => (
-                  <div className="cell">{value}</div>
-                ))}
-              </div>
-            </div>
-            <table>
-              <tr className="yourScores-course-information-wrapper">
-                {allHoles.map((courseHole, courseHoleIndex) => (
-                  <td key={courseHoleIndex} className="yourScores-hole-value">
-                    <div className="cell">{courseHole.Name}</div>
-                    <div className="cell">{courseHole.Distance}</div>
-                    <div className="cell">{courseHole.Par}</div>
-                  </td>
-                ))}
-              </tr>
-              <tr className="yourScores-player-score-map-wrapper">
-                {score.map((hole, holeIndex) => (
-                  <td key={holeIndex} className="yourScores-hole-value">
-                    <div className="cell">{hole.Score}</div>
-                    <FontAwesomeIcon
-                      className={`cell cell-icon ${
-                        hole.FIR
-                          ? 'green-check'
-                          : hole.Par !== 3
-                          ? 'red-x'
-                          : 'gray-minus'
-                      }`}
-                      icon={
-                        hole.FIR
-                          ? faCheckCircle
-                          : hole.Par !== 3
-                          ? faTimesCircle
-                          : faMinusCircle
-                      }
-                    >
-                      {!hole.FIR && hole.Par === 3 ? '-' : null}
-                    </FontAwesomeIcon>
-                    <div className="cell">
-                      {hole.apprDistance ? hole.apprDistance : '-'}
+    <div className="yourScores-wrapper">
+      <Header />
+      <div className="container">
+        <div className="yourScores-round-results-wrapper ">
+          {scores.map((score, scoreIndex) => (
+            <div key={scoreIndex} className="d-flex">
+              <div className="yourScores-round-values-wrapper">
+                <div className="yourScores-course-values-wrapper">
+                  {[
+                    "Hole:",
+                    "Yards:",
+                    "Par:",
+                    "FIR:",
+                    "Appr:",
+                    "GIR:",
+                    "Putts:",
+                    "Score:",
+                  ].map((value, valueIndex) => (
+                    <div key={valueIndex} className="cell">
+                      {value}
                     </div>
-                    <FontAwesomeIcon
-                      className={`cell cell-icon ${
-                        hole.GIR ? 'green-check' : 'red-x'
-                      }`}
-                      icon={hole.GIR ? faCheckCircle : faTimesCircle}
-                    />
-                    <div className="cell">{hole.numberOfPutts}</div>
-                  </td>
-                ))}
-              </tr>
-            </table>
-          </div>
-        ))}
+                  ))}
+                </div>
+              </div>
+              <table>
+                <tr className="yourScores-player-score-map-wrapper">
+                  {score.map((hole, holeIndex) => (
+                    <td key={holeIndex} className="yourScores-hole-value">
+                      <div className="cell">{holeIndex + 1}</div>
+                      <div className="cell">{hole.Distance}</div>
+                      <div className="cell">{hole.Par}</div>
+                      <FontAwesomeIcon
+                        className={`cell cell-icon ${
+                          hole.FIR
+                            ? "green-check"
+                            : hole.Par !== 3
+                            ? "red-x"
+                            : "black-minus"
+                        }`}
+                        icon={
+                          hole.FIR
+                            ? faCheckCircle
+                            : hole.Par !== 3
+                            ? faTimesCircle
+                            : faMinusCircle
+                        }
+                      >
+                        {!hole.FIR && hole.Par === 3 ? "-" : null}
+                      </FontAwesomeIcon>
+                      <div className="cell">
+                        {hole.apprDistance ? (
+                          hole.apprDistance
+                        ) : (
+                          <FontAwesomeIcon icon={faMinusCircle} />
+                        )}
+                      </div>
+                      <FontAwesomeIcon
+                        className={`cell cell-icon ${
+                          hole.GIR ? "green-check" : "red-x"
+                        }`}
+                        icon={hole.GIR ? faCheckCircle : faTimesCircle}
+                      />
+                      <div className="cell">{hole.numberOfPutts}</div>
+
+                      <div className="cell">{hole.Score}</div>
+                    </td>
+                  ))}
+                </tr>
+              </table>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
