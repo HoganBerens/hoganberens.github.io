38fe5dc4e23a47af339daf15b422b92a48940822
Hogan Berens <berenshogan@gmail.com>
2023-05-17 19:26:56 -0500

    testing gh-pages

--- src/components/portfolio/dashboard.js
+++ src/components/portfolio/dashboard.js
@@ -1,13 +1,12 @@
 import React from "react";
 import AboutMe from "./aboutMe";
-import Header from "./header";
 import Projects from "./projects";
 import "./dashboard.css";
 
 const Dashboard = () => {
   return (
     <div className="dashboard-wrapper">
-      <div className="dashboard ">
+      <div className="dashboard-container ">
         <div className="dashboard-background-box" />
         <div className="dashboard-box-wrapper">
           <AboutMe />
