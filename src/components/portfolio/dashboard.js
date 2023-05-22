2656e14148fcb1c738265c0a25dd8d5c3ad5b8d2
Hogan Berens <berenshogan@gmail.com>
2023-05-22 10:23:21 -0500

    gh-pages testing

--- src/components/portfolio/dashboard.js
+++ src/components/portfolio/dashboard.js
@@ -4,6 +4,26 @@ import Projects from "./projects";
 import "./dashboard.css";
 
 const Dashboard = () => {
+  useEffect(() => {
+    localStorage.setItem(
+      "clubs",
+      JSON.stringify([
+        "Driver",
+        "Three Wood",
+        "2 Iron",
+        "4 Iron",
+        "5 Iron",
+        "6 Iron ",
+        "7 Iron",
+        "8 Iron",
+        "9 Iron",
+        "Pitching Wedge",
+        "50 Degree",
+        "54 Degree",
+        "58 Degree",
+      ])
+    );
+  }, []);
   return (
     <div className="dashboard-wrapper">
       <div className="homepage-container">
